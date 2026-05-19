8fdb5f1bc770ae3f13a7846a0154a004b13dfd19
Korchat123 <korchat123@gmail.com>
2026-05-19 16:41:57 +0700

    create backend

---
+++ src/configs/mongodb.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+export async function connectDB() {
+    
+
+const uri =process.env.MONGODB_URI;
+try{
+    await mongoose.connect(uri,{dbName:"serious-spin3"});
+    console.log("MongoDB connected")
+
+}catch(err){
+    console.error("mongodb connection error");
+    
+}
+
+
+
+}
---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { Router } from "express";
+import { router as apiRoutes } from "./routes/index.js";
+import {connectDB} from "./configs/mongodb.js";
+const app=express();
+
+app.use("/api",apiRoutes);
+await connectDB();
+app.listen(PORT);
